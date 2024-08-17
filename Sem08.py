be44696f897155a723dc3051fbe266c7e6a26b03
Sergio Orizano Salvador <svorizano@gmail.com>
2024-08-16 20:26:17 -0500

    cargando archivos

--- Sem08.py
+++ Sem08.py
@@ -4,3 +4,6 @@ import pandas as pd
 
 #Título al app
 st.title("K-Means Clustering con Streamlit")
+
+#Subir archivo de Excel
+uploaded_file = st.file_uploader("Sube un archivo Excel", type=["xlsx"])
