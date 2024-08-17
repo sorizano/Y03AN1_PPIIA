5cf0c9205668abf973a9959a144ab2e4825fa979
Sergio Orizano Salvador <svorizano@gmail.com>
2024-08-16 21:11:54 -0500

    datos 3

--- Sem08.py
+++ Sem08.py
@@ -10,7 +10,7 @@ st.title("K-Means Clustering con Streamlit")
 # Subir archivo de Excel
 uploaded_file = st.file_uploader("Sube un archivo Excel", type=["xlsx"])
 
-if uploaded_file isnotNone:  # CORREGIDO: Espacio entre 'is' y 'not'# Leer archivo Excel
+if uploaded_file isnotNone: 
     df = pd.read_excel(uploaded_file)
 
     st.write("### Vista previa de los datos")
